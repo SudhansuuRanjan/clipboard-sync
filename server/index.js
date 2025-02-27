b093480270354db00817c00388ab94a6ef6db4f5
SudhansuuRanjan <sudhanshuranjan2k18@gmail.com>
2025-02-27 08:31:43 +0530

    fix: update socket.io import and set dynamic port

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const http = require("http");
-const { Server } = require("socket.io");
+const serv = require("socket.io");
 const { v4: uuidv4 } = require("uuid");
 const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
+const io = serv(server, {
     cors: {
         origin: "*"
     }
@@ -55,5 +55,5 @@ io.on("connection", (socket) => {
     });
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
