b6c2c66916749249540e4ab9807afa25f01390e5
SudhansuuRanjan <sudhanshuranjan2k18@gmail.com>
2025-03-02 10:10:32 +0530

    feat: add PWA support with vite-plugin-pwa and update manifest for improved offline capabilities

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,52 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      // add this to cache all the imports
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ["**/*"],
+      },
+      includeAssets: [
+        "**/*",
+      ],
+      manifest: {
+        "name": "ClipSync",
+        "short_name": "ClipSync",
+        "description": "A web app for syncing clipboard across devices.",
+        "icons": [
+          {
+            "src": "\/favicon/favicon-96x96.png",
+            "sizes": "96x96",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "\/favicon/web-app-manifest-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "\/favicon/web-app-manifest-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ],
+        "start_url": "/",
+        "id": "/",
+        "display": "standalone",
+        "theme_color": "#00BFFF",
+        "background_color": "#000000",
+        "orientation": "portrait",
+        "prefer_related_applications": true
+      }
+    })
+  ],
 })
