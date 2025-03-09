1586f7ad61b9a32016ab3a98e536e71349b0c2de
SudhansuuRanjan <sudhanshuranjan2k18@gmail.com>
2025-03-10 02:08:28 +0530

    fixed layout bugs and improved the layout of the input fields

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -75,6 +75,8 @@ export default function App() {
                 setHistory(data);
             })();
         }
+
+        console.clear();
     }, []);
 
     const createSession = async () => {
@@ -278,6 +280,9 @@ export default function App() {
                 }
             })
             .subscribe();
+        
+        // clear console 
+        console.clear();
 
         return () => {
             supabase.removeChannel(channel);
@@ -306,9 +311,9 @@ export default function App() {
                 {isDarkMode ? <Sun size={20} /> : <Moon size={20} />}
             </button>
 
-            <div className={`max-w-5xl lg:max-w-4xl md:max-w-3xl w-full shadow-lg rounded-2xl md:p-6 p-4 space-y-6 
+            <div className={`max-w-5xl lg:max-w-4xl md:max-w-3xl w-full shadow-lg rounded-2xl md:p-6 p-4 space-y-5 
                 ${isDarkMode ? 'bg-gray-900' : 'bg-white'}`}>
-                <h1 className={`text-3xl font-extrabold text-center 
+                <h1 className={`md:text-3xl text-2xl font-extrabold text-center 
                     ${isDarkMode ? 'text-gray-200' : 'text-gray-900'}`}>
                     Clipboard Sync
                 </h1>
@@ -319,7 +324,7 @@ export default function App() {
                 </div>}
 
                 {sessionCode && <div className="w-full flex items-center justify-center flex-col">
-                    <p className={`text-lg text-center items-center 
+                    <div className={`text-lg text-center items-center 
                         ${isDarkMode ? 'text-gray-400' : 'text-gray-600'}`}>
                         Session Code: <strong className={isDarkMode ? 'text-blue-500' : 'text-blue-600'}>{sessionCode.toUpperCase()}</strong>
                         <button
@@ -333,7 +338,7 @@ export default function App() {
                             }}>
                             <LogOut size={17} />
                         </button>
-                    </p>
+                    </div>
                     <p className={`text-sm text-center max-w-xl px-3 
                         ${isDarkMode ? 'text-gray-400' : 'text-gray-900'}`}>
                         (Join on another device using the code to sync clipboard content between devices.)
@@ -342,70 +347,72 @@ export default function App() {
 
                 <div className="flex gap-2">
                     <input
-                        className={`border p-2 rounded-lg flex-1 
+                        className={`border p-2 py-1.5 rounded-lg flex-1 
                             ${isDarkMode ? 'bg-slate-800 border-gray-600 text-gray-200' : 'bg-white border-gray-300 text-gray-900'}`}
                         placeholder="Enter session code to retrieve"
                         value={inputCode.toUpperCase()}
                         onChange={(e) => setInputCode(e.target.value)}
                     />
-                    <button className={`bg-green-500 font-medium hover:bg-green-600 hover:scale-[101%] transition active:bg-green-700 px-4 py-2 rounded-lg ${isDarkMode ? `text-gray-800` : `text-white`}`} onClick={joinSession}>Join</button>
+                    <button className={` font-medium hover:bg-green-600 hover:scale-[101%] transition active:bg-green-700 px-4 py-1.5 rounded-lg ${isDarkMode ? `text-gray-800 bg-green-500` : `text-white bg-green-600`}`} onClick={joinSession}>Join</button>
                 </div>
 
-                <textarea
-                    rows={5}
-                    className={`border p-3 w-full rounded-lg focus:ring-2 focus:ring-blue-400 
+                <div className="flex flex-col gap-3">
+                    <textarea
+                        rows={6}
+                        className={`border p-3 w-full rounded-lg focus:ring-2 focus:ring-blue-400 
                         ${isDarkMode ? 'bg-slate-800 border-gray-600 text-gray-200' : 'bg-gray-50 border-gray-300 text-gray-900'}`}
-                    placeholder="Type or paste clipboard content here..."
-                    value={clipboard}
-                    onChange={(e) => setClipboard(e.target.value)}
-                />
+                        placeholder="Type or paste clipboard content here..."
+                        value={clipboard}
+                        onChange={(e) => setClipboard(e.target.value)}
+                    />
 
-                <div className="flex flex-wrap md:gap-2 gap-1.5">
-                    <input type="file" className="hidden" id="attachfile" accept=".txt" onChange={async (e) => {
-                        const file = e.target.files[0];
-                        // Upload file to Supabase Storage
-                        await UploadFile(file);
-                        // clear file input
-                        e.target.value = null;
-                    }} />
+                    <div className="flex flex-wrap md:gap-2 gap-1.5">
+                        <input type="file" className="hidden" id="attachfile" accept=".txt" onChange={async (e) => {
+                            const file = e.target.files[0];
+                            // Upload file to Supabase Storage
+                            await UploadFile(file);
+                            // clear file input
+                            e.target.value = null;
+                        }} />
 
-                    <label htmlFor="attachfile" className={`flex w-fit items-center gap-2 cursor-pointer ${isDarkMode ? 'bg-gray-800 border-gray-700 text-white active:bg-blue-600' : 'bg-gray-100 text-gray-800 border-gray-300 active:bg-gray-400'}  border  hover:scale-[101%] py-1.5 md:px-4 px-3 rounded-full text-sm`}>
-                        <Paperclip className="text-green-500" size={18} /> Attach File
-                    </label>
+                        <label htmlFor="attachfile" className={`flex w-fit items-center gap-2 cursor-pointer ${isDarkMode ? 'bg-gray-800 border-gray-700 text-white active:bg-blue-600' : 'bg-gray-100 text-gray-800 border-gray-300 active:bg-gray-400'}  border  hover:scale-[101%] py-1.5 md:px-4 px-3 rounded-full text-sm`}>
+                            <Paperclip className="text-green-500" size={18} /> Attach File
+                        </label>
 
-                    <input type="file" className="hidden" id="attachimage" accept="image/*" onChange={async (e) => {
-                        const file = e.target.files[0];
-                        // Upload file to Supabase Storage
-                        await UploadFile(file, "image");
+                        <input type="file" className="hidden" id="attachimage" accept="image/*" onChange={async (e) => {
+                            const file = e.target.files[0];
+                            // Upload file to Supabase Storage
+                            await UploadFile(file, "image");
 
-                        // clear file input
-                        e.target.value = null;
-                    }} />
+                            // clear file input
+                            e.target.value = null;
+                        }} />
 
-                    <label htmlFor="attachimage" className={`flex w-fit items-center gap-2 cursor-pointer border hover:scale-[101%] ${isDarkMode ? 'bg-gray-800 border-gray-700 text-white active:bg-blue-600' : 'bg-gray-100 text-gray-800 border-gray-300 active:bg-gray-400'} py-1.5 md:px-4 px-3 rounded-full text-sm`}>
-                        <FileImage className="text-rose-500" size={18} /> Attach Image
-                    </label>
+                        <label htmlFor="attachimage" className={`flex w-fit items-center gap-2 cursor-pointer border hover:scale-[101%] ${isDarkMode ? 'bg-gray-800 border-gray-700 text-white active:bg-blue-600' : 'bg-gray-100 text-gray-800 border-gray-300 active:bg-gray-400'} py-1.5 md:px-4 px-3 rounded-full text-sm`}>
+                            <FileImage className="text-rose-500" size={18} /> Attach Image
+                        </label>
 
-                    <input type="file" className="hidden" id="file" accept=".txt" onChange={(e) => {
-                        const file = e.target.files[0];
-                        if (!file) return;
-                        setFile(file);
+                        <input type="file" className="hidden" id="file" accept=".txt" onChange={(e) => {
+                            const file = e.target.files[0];
+                            if (!file) return;
+                            setFile(file);
 
-                        const reader = new FileReader();
-                        reader.onload = () => {
-                            const text = reader.result;
-                            setClipboard(text);
-                        };
-                        reader.readAsText(file);
-                        toast.success("File selected successfully!");
+                            const reader = new FileReader();
+                            reader.onload = () => {
+                                const text = reader.result;
+                                setClipboard(text);
+                            };
+                            reader.readAsText(file);
+                            toast.success("File selected successfully!");
 
-                        // clear file input
-                        e.target.value = null;
-                    }} />
+                            // clear file input
+                            e.target.value = null;
+                        }} />
 
-                    <label htmlFor="file" className={`hidden md:flex w-fit items-center gap-2 cursor-pointer border hover:scale-[101%] ${isDarkMode ? 'bg-gray-800 border-gray-700 text-white active:bg-blue-600' : 'bg-gray-100 text-gray-800 border-gray-300 active:bg-gray-400'} py-1.5 md:px-4 px-3 rounded-full text-sm`}>
-                        <FileUp className="text-blue-500" size={18} /> Import Text File
-                    </label>
+                        <label htmlFor="file" className={`hidden md:flex w-fit items-center gap-2 cursor-pointer border hover:scale-[101%] ${isDarkMode ? 'bg-gray-800 border-gray-700 text-white active:bg-blue-600' : 'bg-gray-100 text-gray-800 border-gray-300 active:bg-gray-400'} py-1.5 md:px-4 px-3 rounded-full text-sm`}>
+                            <FileUp className="text-blue-500" size={18} /> Import Text File
+                        </label>
+                    </div>
                 </div>
 
 
@@ -427,15 +434,15 @@ export default function App() {
 
 
                 <div className="flex gap-2 flex-wrap">
-                    <button className="flex-1 min-w-32 flex items-center justify-center transition gap-2 bg-blue-500 hover:bg-blue-600 hover:scale-[101%] active:bg-blue-700 text-white py-2 rounded-lg" onClick={addClipboardText}><ClipboardList size={18} /> Paste Text</button>
-                    <button className="flex-1 min-w-32 flex items-center justify-center transition gap-2 bg-red-500 hover:bg-red-600 hover:scale-[101%] active:bg-red-700 text-white py-2 rounded-lg" onClick={() => {
+                    <button className={`flex-1 min-w-32 flex items-center justify-center transition gap-2 bg-blue-500 hover:bg-blue-600 hover:scale-[101%] active:bg-blue-700 ${isDarkMode ? 'text-black' : "text-white"} py-2 rounded-lg`} onClick={addClipboardText}><ClipboardList size={18} /> Paste Text</button>
+                    <button className={`flex-1 min-w-32 flex items-center justify-center transition gap-2 bg-red-500 hover:bg-red-600 hover:scale-[101%] active:bg-red-700  ${isDarkMode ? 'text-black' : "text-white"} py-2 rounded-lg`} onClick={() => {
                         setClipboard("");
                         supabase.storage.from("clipboard").remove([fileUrl.name]);
                         setFileUrl(null);
                         setFile(null);
                         toast.success("Clipboard cleared successfully!");
                     }}><Trash2 size={18} /> Clear</button>
-                    <button className="flex-1 min-w-48 flex items-center justify-center transition gap-2 bg-green-500 hover:bg-green-600 hover:scale-[101%] active:bg-green-700 text-white py-2 rounded-lg" onClick={updateClipboard}><Send size={18} /> Send to Clipboard</button>
+                    <button className={`flex-1 min-w-48 flex items-center justify-center transition gap-2 hover:bg-green-600 hover:scale-[101%] active:bg-green-700  ${isDarkMode ? 'text-black bg-green-500' : "text-white bg-green-600"} py-2 rounded-lg`} onClick={updateClipboard}><Send size={18} /> Send to Clipboard</button>
                 </div>
             </div>
 
@@ -447,14 +454,14 @@ export default function App() {
                             ${isDarkMode ? 'text-gray-200' : 'text-gray-900'}`}>
                             Clipboard History
                         </h2>
-                        <button className="text-red-500 active:text-red-700 active:scale-95 flex gap-2" onClick={deleteAll}><Trash2Icon size={19} /></button>
+                        <button aria-label="Delete All Clipboards" className="text-red-500 active:text-red-700 active:scale-95 flex gap-2" onClick={deleteAll}><Trash2Icon size={19} /></button>
                     </div>
                     <ul className="mt-4 space-y-2">
                         {history.map((item, index) => (
                             <li key={item.id} className={`flex justify-between items-start p-2 py-2.5 gap-2 rounded-lg shadow 
                                 ${isDarkMode ? 'bg-slate-800' : 'bg-gray-50'}`}>
                                 <div className="flex gap-1 items-start">
-                                    <button className="text-blue-500 transition" onClick={() => toggleExpand(item.id)}>
+                                    <button aria-label="Expand Content" className="text-blue-500 transition" onClick={() => toggleExpand(item.id)}>
                                         {!expandedId ? <ChevronRight size={18} /> : <ChevronDown size={19} />}
                                     </button>
                                     <div className="flex flex-col">
@@ -474,7 +481,7 @@ export default function App() {
                                                     >
 
                                                         {/* truncate file path (include first 5 letters last 2 letters with .extention if filename is large else show it in full) */}
-                                                        {item.file.path.length > 34 ? item.file.path.substring(6, 10) + "..." + item.file.path.substring(item.file.path.length - 7) : item.file.path.substring(6) }
+                                                        {item.file.path.length > 34 ? item.file.path.substring(6, 10) + "..." + item.file.path.substring(item.file.path.length - 7) : item.file.path.substring(6)}
                                                     </a>
                                                 }
                                             </div>
@@ -482,8 +489,8 @@ export default function App() {
                                     </div>
                                 </div>
                                 <div className="flex items-center gap-3">
-                                    <button className="text-green-500 active:text-green-700 active:scale-95" onClick={() => handleEdit(item.id)}><Edit size={19} /></button>
-                                    <button className="text-blue-500 active:text-blue-700 active:scale-95" onClick={() => copyToClipboard(item.content)}><Copy size={19} /></button>
+                                    <button aria-label="Edit Clipboard" className="text-green-500 active:text-green-700 active:scale-95" onClick={() => handleEdit(item.id)}><Edit size={19} /></button>
+                                    <button aria-label="Copy Content" className="text-blue-500 active:text-blue-700 active:scale-95" onClick={() => copyToClipboard(item.content)}><Copy size={19} /></button>
                                 </div>
                             </li>
                         ))}
